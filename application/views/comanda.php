57ebced0fa18bf915c40014c9d41e594bdca2cae
leafarsaid <rafael78@gmail.com>
2014-07-31 18:44:43 -0300

    bootstrap

--- application/views/comanda.php
+++ application/views/comanda.php
@@ -1,13 +1,43 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Comanda</title>
     <!-- Bootstrap -->
     <link href="<?php echo base_url('bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet" media="screen">
+    <link href="<?php echo base_url('bootstrap/css/bootstrap-responsive.css'); ?>" rel="stylesheet">
   </head>
   <body>
-    <h1>Hello, world!</h1>
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>
+    <div class="container">
+        <div class="navbar navbar-inverse navbar-fixed-top">
+	      <div class="navbar-inner">
+	        <div class="container-fluid">
+	          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="brand" href="#">Meu Pedido</a>
+	          <div class="nav-collapse collapse">
+	            <p class="navbar-text pull-right">
+	              Logado como <a href="#" class="navbar-link">RafaelDias</a>
+	            </p>
+	            <ul class="nav">
+	              <li class="active"><a href="#">Home</a></li>
+	              <li><a href="#about">About</a></li>
+	              <li><a href="#contact">Contact</a></li>
+	            </ul>
+	          </div><!--/.nav-collapse -->
+	        </div>
+	      </div>
+	    </div>
+	    
+	    
+	    
+	    <div class="row-fluid">
+	    	<div style="width: 50px; height: 50px; background-color: red; border: 5px; border-color: white;"></div>
+	    </div>
+    </div>
   </body>
 </html>
