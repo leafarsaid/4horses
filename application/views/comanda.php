30e2075021884d27e8a84455b1f779d007927454
leafarsaid <rafael78@gmail.com>
2014-08-01 16:25:55 -0300

    bootstrap

--- application/views/comanda.php
+++ application/views/comanda.php
@@ -5,38 +5,100 @@
     <!-- Bootstrap -->
     <link href="<?php echo base_url('bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet" media="screen">
     <link href="<?php echo base_url('bootstrap/css/bootstrap-responsive.css'); ?>" rel="stylesheet">
+    <style>
+    	body{
+    		background-image: url("<?php echo base_url('imgs/bg480x800.png'); ?>");
+    		background-position:left top;
+    	}
+    	.teste{
+    		background-color: red;
+    		min-height:100px;
+    		width: 100px; 
+    		height: 100px; 
+    		border-width: 10px;
+    		border-style:solid;
+    		border-color: #ffffff;
+    		display: inline-block;
+    		margin: 17px;
+    	}
+    </style>
   </head>
   <body>
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>
+        
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Meu Pedido</a>
+          <div class="nav-collapse collapse nav-collapse-fixed-top">
+            <p class="navbar-text pull-right">
+              Logado como <a href="#" class="navbar-link">RafaelDias</a>
+            </p>
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>    
+    
     <div class="container">
-        <div class="navbar navbar-inverse navbar-fixed-top">
-	      <div class="navbar-inner">
-	        <div class="container-fluid">
-	          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	          </button>
-	          <a class="brand" href="#">Meu Pedido</a>
-	          <div class="nav-collapse collapse">
-	            <p class="navbar-text pull-right">
-	              Logado como <a href="#" class="navbar-link">RafaelDias</a>
-	            </p>
-	            <ul class="nav">
-	              <li class="active"><a href="#">Home</a></li>
-	              <li><a href="#about">About</a></li>
-	              <li><a href="#contact">Contact</a></li>
-	            </ul>
-	          </div><!--/.nav-collapse -->
-	        </div>
-	      </div>
-	    </div>
-	    
-	    
-	    
 	    <div class="row-fluid">
-	    	<div style="width: 50px; height: 50px; background-color: red; border: 5px; border-color: white;"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
+	    	<div class="teste"></div>
 	    </div>
     </div>
   </body>
